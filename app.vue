0e567e2e6220025d4803207e9c59fd9565de7441
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-07 21:45:49 +0900

    feat: 変数でテーマを変更

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="dark">
+  <div :class="{ dark: darkMode }">
     <h1 class="text-red-500 dark:text-dim-500">Twitter clone</h1>
   </div>
 </template>
+<script setup>
+const darkMode = ref(true);
+</script>
