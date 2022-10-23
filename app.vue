3ef46c4d6e89630cbc44562a5c480c0df068281e
shimizu-saffle <shimizu.saffle@gmail.com>
2022-10-23 09:31:43 +0900

    feat: ロゴ周りの枠線を削除

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      >
-        <!-- Left sidebar -->
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
         <div
-          class="border-2 border-red-500 md:block xs-col-span-1 xl:col-span-2"
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
-          <div class="sticky top-0">
-            <SidebarLeft />
+          <!-- Left sidebar -->
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
         </div>
       </div>
