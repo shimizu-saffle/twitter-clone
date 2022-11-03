c47b5c40b46b01c85cba53a3fbccf59572d4d179
shimizu-saffle <shimizu.saffle@gmail.com>
2022-11-03 18:16:00 +0900

    feat: ダークモード時のカラー

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="p-2 my-2 hover:bg-blue-50 hover:rounded-full w-min">
+    <div
+      class="p-2 my-2 hover:bg-blue-50 hover:rounded-full w-min dark:hover:bg-white/20"
+    >
       <nuxt-link to="/">
         <div class="w-8 h-8">
           <LogoTwitter />
