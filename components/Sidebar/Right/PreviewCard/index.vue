4a8f6d64306a2588ea623aca4a22e96d5bace811
shimizu-saffle <shimizu.saffle@gmail.com>
2022-12-11 18:23:34 +0900

    feat: add show more on PreviewCard

--- components/Sidebar/Right/PreviewCard/index.vue
+++ components/Sidebar/Right/PreviewCard/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div
-    class="m-2 border rounded-2xl bg-gray-50 dark:bg-dim-700"
+    class="m-2 overflow-hidden border rounded-2xl bg-gray-50 dark:bg-dim-700"
     :class="twittersBorderColor"
   >
-    <h1 class="p-3">
+    <h1
+      class="p-3 text-xl font-extrabold text-gray-900 border-b dart:text-white"
+    >
       {{ props.title }}
     </h1>
+
+    <slot> </slot>
+
+    <div class="p-3 text-sm text-blue-400 cursor-pointer hover:bg-gray-100">
+      Show more
+    </div>
   </div>
 </template>
 
