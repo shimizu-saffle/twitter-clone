0d55936e839a46d744eb465fbe06da80cd52fddb
shimizu-saffle <shimizu.saffle@gmail.com>
2022-12-11 16:58:55 +0900

    feat: main content place holder

--- app.vue
+++ app.vue
@@ -13,6 +13,9 @@
           </div>
 
           <!-- Main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>asd</h1>
+          </main>
 
           <!-- Right sidebar -->
         </div>
